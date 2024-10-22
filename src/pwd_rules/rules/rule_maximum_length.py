714e4e6ad4943aca7cf42810674a469bab28366a
Henrik <henrik.alexander.sydow@gmail.com>
2024-10-22 08:31:38 +0200

    add: rule max length

---
+++ src/pwd_rules/rules/rule_maximum_length.py
@@ -0,0 +1,9 @@
+from src.pwd_rules.pwd_rule_base import PwdRuleBase
+
+
+class RuleMaximumLength(PwdRuleBase):
+    MaxLength: int = 30
+    Description = f"Password must not contain more than {MaxLength} characters."
+
+    def check(self, password: str) -> bool:
+        return len(password) < RuleMaximumLength.MaxLength
