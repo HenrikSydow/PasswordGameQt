e4871a71ff3c5e88fb1e534c12b3b19b2c0b8722
Henrik <henrik.alexander.sydow@gmail.com>
2024-10-22 08:37:41 +0200

    fix: calc result rule

--- src/pwd_rules/rules/rule_contains_calc_result.py
+++ src/pwd_rules/rules/rule_contains_calc_result.py
@@ -3,7 +3,7 @@ from src.pwd_rules.pwd_rule_base import PwdRuleBase
 
 class RuleContainsCalcResult(PwdRuleBase):
     Formula: str = "((2.5 * 10 + (-5)) / 4) / 5 + 47"
-    Result: str = str(eval(Formula))
+    Result: str = str(int(eval(Formula)))
     Description = f"Password must contain the result of this formula:\n{Formula}"
 
     def check(self, password: str) -> bool:
