cf0076cf89c1c16529bc912a379dde127169cf33
Henrik <henrik.alexander.sydow@gmail.com>
2024-10-22 08:52:36 +0200

    fix: syntax error

--- src/composition_root.py
+++ src/composition_root.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
             RuleContainsChineseChar(),
             RuleContainsWeirdCharSequence(),
             RuleFirstCharMatchesLast(),
-            RuleEvenNumber(g)
+            RuleEvenNumber()
         ]
     )
 
