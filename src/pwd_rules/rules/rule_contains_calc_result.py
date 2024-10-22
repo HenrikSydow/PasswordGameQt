6f44e9f157d5b920d751286654fd6edca6e73058
Henrik <henrik.alexander.sydow@gmail.com>
2024-10-22 08:33:56 +0200

    fix: data type of result

--- src/pwd_rules/rules/rule_contains_calc_result.py
+++ src/pwd_rules/rules/rule_contains_calc_result.py
@@ -2,8 +2,8 @@ from src.pwd_rules.pwd_rule_base import PwdRuleBase
 
 
 class RuleContainsCalcResult(PwdRuleBase):
-    Formula = "((2.5 * 10 + (-5)) / 4) / 5 + 47"
-    Result = eval(Formula)
+    Formula: str = "((2.5 * 10 + (-5)) / 4) / 5 + 47"
+    Result: str = str(eval(Formula))
     Description = f"Password must contain the result of this formula:\n{Formula}"
 
     def check(self, password: str) -> bool:
